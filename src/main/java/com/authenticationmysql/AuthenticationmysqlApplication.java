9e194815312ebfa9afea11f1db3d2de5d7bb70e9
TOKPE627 <75177651+TOKPE627@users.noreply.github.com>
2021-04-11 21:36:34 +0000

    Test of Autentication in Spring Boot and Mysql

---
+++ src/main/java/com/authenticationmysql/AuthenticationmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.authenticationmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationmysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinerimmobilierback/AuthenticationmysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinerimmobilierback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationmysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
